dfd93dd1d27ed50942de265c3180144bb3eb7308
Angel Borja <borjaangel244@gmail.com>
2023-07-07 09:26:55 -0400

    7 out of 9 done

--- script.js
+++ script.js
@@ -1,36 +1,75 @@
-function printName(){
-    console.log("my name is Angel");
+function printName() {
+  console.log("my name is Angel");
 }
 
-function greetUser(userName){
-    console.log("hello, " + userName);
+function greetUser(userName) {
+  console.log("hello, " + userName);
 }
 
-function buyCoffee(dollars){
-    const price = 4;
-    const remainder = dollars - price;
-    return remainder;
+function buyCoffee(dollars) {
+  const price = 4;
+  const remainder = dollars - price;
+  return remainder;
 }
 
-function myHobbies(hobbyArray){
-    return hobbyArray;
+function myHobbies(hobbyArray) {
+  return hobbyArray;
 }
 
-function checkWeather(weatherCondition, temperature){
-    hotAndHumid = "wear shorts and t shirt";
-    rainyAndCold = "wear jeans and a coat";
-    warmAndBreezy = "beautiful day! wear either shorts or jeans and either short sleeve or longsleeve" 
+function checkWeather(weatherCondition, temperature) {
+  if (weatherCondition === "rain") {
+    if (temperature >= 40) {
+      return "its a little warmer, but be dressed to get wet";
+    } else {
+      return "it is freezing, get your booty inside or dress warm";
+    }
+  } else if (weatherCondition === "sun") {
+    if (temperature >= 70) {
+      return "it is hot, wear a t shirt and shorts";
+    } else {
+      return "it is fine to wear pants, just be prepared in case of humidity";
+    }
+  } else if (weatherCondition === "humid") {
+    if (temperature >= 80) {
+      return "stay inside and dont even bother with this heat, but drink a lot of water";
+    } else {
+      return "slightly cooler, but be careful";
+    }
+  } else {
+    return "that weather does not exist dummy";
+  }
 }
+
+function areaOfTriangle(base, height){
+  const area = (base * height) / 2;
+  return area;
+}
+
+function diceRoller(){
+  min = 2;
+  max = 12;
+  result = Math.floor(Math.random() * (max - min)) + min;
+  return result;
+}
+
+function helloAliyahorHakeem(){}
+
+function getInitials(){}
 
 printName();
 
-let userName = "IttzAngel";
+userName = "IttzAngel";
 greetUser(userName);
 
 dollars = 10;
 moneyLeft = buyCoffee(dollars);
 console.log(moneyLeft);
 
-const hobbyArray = ["Gaming", "Coding", "Gym"];
+hobbyArray = ["Gaming", "Coding", "Gym"];
 console.log(myHobbies(hobbyArray));
 
+console.log(checkWeather("sun", 90));
+
+console.log(areaOfTriangle(10, 5));
+
+console.log(diceRoller());
